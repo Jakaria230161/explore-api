0704f225c4acaed71a4143b909e6eb64c226a1f1
Jakaria230161 <jakariamahmud016@gmail.com>
2022-08-29 06:59:54 +0600

    exploring fetch

--- js/fetch.js
+++ js/fetch.js
@@ -1,2 +1,24 @@
+// fetch exploring here
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(response => console.log(response)); // .json is not similar but close to  JSON.pares.
+    // .then(json => console.log(json))
 
-console.log("fetching");
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//     .then(res => res.json())
+//     .then(json => console.log(json));
+
+
+function loadData() {
+    const url = "https://jsonplaceholder.typicode.com/todos/1";
+    fetch(url)
+        .then(response => response.json())
+        .then(json => console.log(json))
+}
+
+
+function fetchData() {
+    const url1 = "https://jsonplaceholder.typicode.com/todos/1";
+    fetch(url1)
+        .then(res => res.json())
+    .then(data => console.log(data))
+}
