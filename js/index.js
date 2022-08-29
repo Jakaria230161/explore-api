35b3fdffd24a9dfc029a98832c6ef91e7b562b24
Jakaria230161 <jakariamahmud016@gmail.com>
2022-08-29 09:03:45 +0600

    patch,delete

--- js/index.js
+++ js/index.js
@@ -33,3 +33,44 @@ function displayPost(post) {
     }
 
 }
+
+
+// delete Post
+function deletePost() {
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+    method: 'DELETE',
+});
+}
+
+
+// patch post
+
+function patchPost() {
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+    method: 'PATCH',
+    body: JSON.stringify({
+    title: 'foo',
+    }),
+    headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+    },
+})
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
+
+function createPost() {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+    }),
+    headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+    },
+})
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
